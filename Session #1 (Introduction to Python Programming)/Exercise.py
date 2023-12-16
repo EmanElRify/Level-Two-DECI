92e9ec6084d32d7b3f0248bfab09e4adaa995c39
Eman ElRify <52162779+EmanElRify@users.noreply.github.com>
2023-12-16 18:13:08 +0200

    first session

---
+++ Session #1 (Introduction to Python Programming)/Exercise.py
@@ -0,0 +1,33 @@
+# x = 3
+
+# x = 2*4
+
+# x = 3+15
+
+# x = 3*"abc"
+
+# x = 3+"x"
+
+# x = 15 > 20
+
+x = 15 >= 15
+
+# x = True or False
+
+# x = True and False
+
+x = "abc"+"3"
+
+print(x)
+
+def add(a,b):
+    """a function used to add two integers
+
+    Args:
+        a (int): the first number
+        b (int): the second number
+
+    Returns:
+        the sum of a and b
+    """
+    return (a +b)
---
+++ Session #1 (Introduction to Python Programming)/recap.py
@@ -0,0 +1,24 @@
+# import turtle
+
+# x = turtle.Turtle()
+# x.color("red")
+# x = 5 
+# x = 5.4
+# x = (x + 1)
+# print(x)
+# x = [1,2,3]
+# x = (1,'x',3.4)
+# x = {'student name': "ahmed", 'student age' : 18}
+# for element in [1,2,3,4]:
+#     x.fd(100)
+#     x.rt(90)
+x = 3
+y = 3
+x = x * 2
+print (x)
+y *= 2
+print (x)
+
+    
+    
+    
